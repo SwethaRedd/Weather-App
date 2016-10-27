7c9fe2addd863b727c9a83a8e687844f165a756b
SwethaRedd <swethareddy.nadipi@gmail.com>
2016-10-26 21:16:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,145 @@
 
-function displayWeather(){
+var currentLocation = 20770;
+var areCoordinatesAvailable = false;
 
-	var city = document.getElementById("main").value;
-
-	var xmlhttp = new XMLHttpRequest(); //Creates a new XMLHttpRequest object (to retrieve data from a web server)
-	var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=8ee882673408269ed12c658c1a82db6c&units=metric";
-	xmlhttp.onreadystatechange = function() {
-
-    	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { 
-
-        	var myObject = JSON.parse(xmlhttp.responseText); //responseText	Returns the response data as a string
-
-        	var resultsArray = myObject.weather;
-
-        	var html = '';
-			for(var i = 0;i<resultsArray.length;i++){
-				html += '<div class="result">'
-					+'Temparature: '+resultsArray[i].main+'<br>'
-					+'temp: '+myObject.icon+ '<br>'
-					+'Humidity: '+resultsArray[i].description+ '<br>'
-					+'Icon: '+myObject.main.temp+ '<br>'
-					+'Min-temp: '+myObject.main.temp_min+ '<br>'
-					+'Max-temp: '+myObject.main.temp_max+ '<br>'
-					+'Sea-Level: '+myObject.main.sea_level+ '<br>'
-					+'Grnd-Level: '+myObject.main.grnd_level+ '<br>'
-					+'Wind: '+myObject.wind.speed+ '<br>'
-					+'Clouds: '+myObject.clouds.all+ '<br>'
-					+'Country: '+myObject.sys.country+ '<br>'
-					+'Sunrise: '+myObject.sys.sunrise+ '<br>'
-					+'Sunset: '+myObject.sys.sunset+ '<br>'
-
-
-
-					// +'MapUrl: '+resultsArray[i].MapUrl+'<br>'	
-					// +'Address: '+resultsArray[i].Address+', '+resultsArray[i].City+' '+resultsArray[i].State+'<br>'
-					// +'Phone: '+resultsArray[i].Phone+'
-					'</div>';
-
-
-// html=html+'<tr><td><img src="'+img+'" ></td><td><b><a href="/city/'+JSONobject.list[i].id+'">'+ name+'</a></b>'+
-// ' <img src="'+flag+'" >  <b><i>' +text+'</i> </b>' + 
-// '<p>  <span class="badge badge-info">'+temp +'°С </span> ' +
-// 'temperature from '+tmin+' to '+tmax+'°С, wind '+gust+ 'm/s. clouds '+cloud+'%, ' +pressure+
-// ' hpa</p><p>Geo coords  <a href="/Maps?zoom=12&lat='+JSONobject.list[i].coord.lat+'&lon='+JSONobject.list[i].coord.lon+'&layers=B0FTTFF">[ '+
-//  JSONobject.list[i].coord.lon+", "+ JSONobject.list[i].coord.lat +' ]</a></p></td></tr>';
-
-//   }
-
-
-
-
-
-
-			}
-			document.getElementById("results").innerHTML = html;
-    	}
-	};
-	//To send a request to a server, we use the open() and send() methods of the XMLHttpRequest object
-	xmlhttp.open("GET", url, true);	
-	xmlhttp.send();	 //Sends the request to the server Used for GET requests
-	
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
 }
-// #main 
 
-function hourlyWeather(){
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
 
-	var city = document.getElementById("hourly").value;
+function getStored() {
+	var storedNames = JSON.parse(localStorage.getItem("names"));
+	if(storedNames != null){
+		var html = '<div class="panel panel-info"><div class="panel-heading">Previous Search</div><div class="panel-body">';
 
-	var xmlhttp = new XMLHttpRequest(); //Creates a new XMLHttpRequest object (to retrieve data from a web server)
-	var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+city+"&mode=xml&units=metric&cnt=7&APPID=8ee882673408269ed12c658c1a82db6c&units=metric";
-	xmlhttp.onreadystatechange = function() {
+		for(var i=0;i<storedNames.length;i++){
+			html += ' <button type="button" class="btn btn-primary" onclick="getWeatherFor(\''+storedNames[i]+'\')">'+storedNames[i]+'</button> ';
+		}
+		html += '</div></div>';
+		$("#otherLocations").html(html);
+	}
+}
 
-    	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { 
+getStored();
 
-        	var myObject1 = JSON.parse(xmlhttp.responseText); //responseText	Returns the response data as a string
+$("#weatherNew").submit(function( event ) {
+  	event.preventDefault();
+  	var location = $("#newLocation").val();
+  	var storedNames = JSON.parse(localStorage.getItem("names"));
+  	if(storedNames == null){
+  		var names = [];
+		names.push(location);
+		localStorage.setItem("names", JSON.stringify(names));
+		console.log("no");
+  	} else {
+  		console.log("yes");
+  		if(!storedNames.contains(location)){
+  			storedNames.push(location);
+			localStorage.setItem("names", JSON.stringify(storedNames));
+  		}
+  	}
 
-        	var resultsArray1 = myObject1.list;
 
-        	var html1 = '';
-			for(var i = 0;i<resultsArray.length;i++){
-				html1 += '<div class="result">'
-					+'Main: '+resultsArray1[i].main+'<br>'
-					+'Weather: '+resultsArray1[i].weather+ '<br>'
-					+'Clouds: '+resultsArray1.clouds+ '<br>'
-					// +'MapUrl: '+resultsArray[i].MapUrl+'<br>'	
-					// +'Address: '+resultsArray[i].Address+', '+resultsArray[i].City+' '+resultsArray[i].State+'<br>'
-					// +'Phone: '+resultsArray[i].Phone+'
-					'</div>';
+	console.log("Stored"+storedNames);
+  	console.log(location);
+	getWeatherFor(location);
+
+	getStored();
+});
+
+function getWeatherFor(locationName){
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+locationName+",us&APPID=94479d3e62bc9e274be58066c734148d&units=imperial", function( data ) {
+		drawData(data);
+	});
+}
+
+function showPosition(position) {
+	if(position.coords.latitude){
+
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;	
+		localStorage.setItem("localLat",lat);		
+		localStorage.setItem("localLng",lng);
+		setWeatherFor(lat, lng);
+	} else {
+		if(localStorage.getItem("localLat")){
+			setWeatherFor(localStorage.getItem("localLat"), localStorage.getItem("localLng"));
+		} else {
+
+		}
+	}
+}
+
+function setWeatherFor(lat, lng){
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&APPID=94479d3e62bc9e274be58066c734148d&units=imperial", function( data ) {
+		drawData(data);
+	});
+}
+
+function drawData(data){
+	$("#location").html("Weather in the city of "+data.name + " <br><br> <img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+ data.main.temp +"&deg;F");
+	$("#thelocationdaily").html(data.name);
+	$("#thelocationhour").html(data.name);
+	$('#thelocationdetails').html(data.name);
+
+	$('#currentPressure').html(data.main.pressure +" in");
+	$('#currentHumidity').html(data.main.humidity +"%");
+	$('#currentMin').html(data.main.temp_min +"&deg;F");
+	$('#currentMax').html(data.main.temp_max +"&deg;F");
+
+	var lat = data.coord.lat;
+	var lon = data.coord.lon;
+
+	$.getJSON("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=94479d3e62bc9e274be58066c734148d&units=metric", function( hourdata ) {
+		var html = "";
+		var details = hourdata.list;
+		html += "<tr>";
+		for(var i = 0;i<details.length;i++){
+			if(i%3 == 0){
+				html += "</tr><tr>";
 			}
-			document.getElementById("results").innerHTML = html;
-    	}
-	};
-	//To send a request to a server, we use the open() and send() methods of the XMLHttpRequest object
-	xmlhttp.open("GET", url, true);	
-	xmlhttp.send();	 //Sends the request to the server Used for GET requests
-	
-} // #hourly
+			html +="<td>"+details[i].dt_txt+"</td><td><img src='http://openweathermap.org/img/w/"+details[i].weather[0].icon+".png'>"+details[i].main.temp+"&deg;F</td>";
+		}
+		$("#hourlyData").html(html);
+	});
 
 
-
-function dailyWeather(){
-
-	var city = document.getElementById("daily").value;
-
-	var xmlhttp = new XMLHttpRequest(); //Creates a new XMLHttpRequest object (to retrieve data from a web server)
-	var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+city+"&mode=xml&units=metric&cnt=7&APPID=8ee882673408269ed12c658c1a82db6c&units=metric";
-	xmlhttp.onreadystatechange = function() {
-
-    	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { 
-
-        	var myObject2 = JSON.parse(xmlhttp.responseText); //responseText	Returns the response data as a string
-
-        	var resultsArray = myObject2.list;
-
-        	var html = '';
-			for(var i = 0;i<resultsArray.length;i++){
-				html += '<div class="result">'
-					+'Temparature: '+resultsArray[i].pressure+'<br>'
-					+'Weather: '+resultsArray[i].humidity+ '<br>'
-					+'Clouds: '+resultsArray[i].weather+ '<br>'
-					// +'MapUrl: '+resultsArray[i].MapUrl+'<br>'	
-					// +'Address: '+resultsArray[i].Address+', '+resultsArray[i].City+' '+resultsArray[i].State+'<br>'
-					// +'Phone: '+resultsArray[i].Phone+'
-					'</div>';
+	$.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?lat="+lat+"&lon="+lon+"&appid=94479d3e62bc9e274be58066c734148d&units=metric", function( dailydata ) {
+		var html = "";
+		var details = dailydata.list;
+		html += "<tr>";
+		for(var i = 0;i<details.length;i++){
+			if(i%3 == 0){
+				html += "</tr><tr>";
 			}
-			document.getElementById("results").innerHTML = html;
-    	}
-	};
-	//To send a request to a server, we use the open() and send() methods of the XMLHttpRequest object
-	xmlhttp.open("GET", url, true);	
-	xmlhttp.send();	 //Sends the request to the server Used for GET requests
-	
-} // #daily
+			var date = new Date(details[i].dt*1000);
+			var thedate = date.getDate();
+			var theMonth = date.getMonth();
+			var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+
+			html +="<td>"+thedate +" "+months[theMonth]+"</td><td><img src='http://openweathermap.org/img/w/"+details[i].weather[0].icon+".png'>"+details[i].temp.day+"&deg;F</td>";
+		}
+		$("#dailyData").html(html);
+	});
+
+}
+
+function setDefaultWeather(){
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=20770&units=metric&appid=94479d3e62bc9e274be58066c734148d", function( data ) {
+		drawData(data);
+	});
+}
+
+getLocation();
